d50e189fd21c4a63923e557206bbf3c3d248d68d
Matt Strube <mattstrube@Strube-MBP.localdomain>
2025-11-03 08:38:50 -0800

    FIX: Fix import error in data_providers/__init__.py - use local fmp_provider module

--- data_providers/__init__.py
+++ data_providers/__init__.py
@@ -1,14 +1,5 @@
-# Compatibility shim - redirects to new location
-import sys
-from pathlib import Path
-
-# Add new location to path
-new_path = Path(__file__).parent.parent.parent / 'trading_models' / 'shared' / 'data_providers'
-if str(new_path) not in sys.path:
-    sys.path.insert(0, str(new_path))
-
-# Re-export
-from fmp_provider import FMPProvider
+# Import from local module
+from .fmp_provider import FMPProvider
 
 __all__ = ['FMPProvider']
 
